15e65ed88cd7f87e596bc506ec3d1fbcb50a19d0
lamekino <zoey.j.mcbride@gmail.com>
2024-06-28 21:04:20 -0400

    cleanup

--- src/notification/windows.py
+++ src/notification/windows.py
@@ -12,47 +12,52 @@ ScriptBlockPS1 = str
 Command = str
 Milliseconds = int
 
+ICO_PATH = Path(r"C:\Windows\SysWOW64\OneDrive.ico")
+
 
 class WindowsNotification(Sender):
-    def __init__(self):
-        # TODO: make this my own icon
-        self.icon = Path(r"C:\Windows\SysWOW64\OneDrive.ico")
-
-    def notification_cmd(self,
-                         title: str,
-                         text: str,
-                         time: Milliseconds = 20000) -> Command:
-        def new_icon(path: Path) -> ScriptBlockPS1:
-            assert path.exists()
-            assert path.is_file()
-
-            return f"[System.Drawing.Icon]::new('{str(path)}')"
-
-        def new_balloon_tip_icon(value: BalloonTip) -> ScriptBlockPS1:
-            return f"[System.Windows.Forms.ToolTipIcon]::{value}"
-
-        def script_block(*cmds: Command) -> ScriptBlockPS1:
-            return "{{{}}}".format(";".join(cmds))
-
-        OBJ_ID = "notifier"
-        return "powershell.exe \"& {}\"".format(script_block(
-            # import the .NET class
-            "Add-Type -AssemblyName System.Windows.Forms",
-            # create the notification object
-            f"$global:{OBJ_ID} = New-Object System.Windows.Forms.NotifyIcon",
-
-            # set the icons
-            f"${OBJ_ID}.Icon = {new_icon(self.icon)}",
-            f"${OBJ_ID}.BalloonTipIcon = {new_balloon_tip_icon("None")}",
-            # set the titles
-            f"${OBJ_ID}.BalloonTipTitle = '{title}'",
-            f"${OBJ_ID}.BalloonTipText = '{text}'",
-            # enable notification display
-            f"${OBJ_ID}.Visible = $true",
-
-            # set time in milliseconds and display the notification
-            f"${OBJ_ID}.ShowBalloonTip({time})",
-        ))
-
     def send(self, title: str, text: str) -> None:
-        subprocess.call(self.notification_cmd(title, text), shell=True)
+        subprocess.call(notification_cmd(title, text, ICO_PATH),
+                        shell=True)
+
+
+def notification_cmd(
+        title: str,
+        text: str,
+        icon: Path,
+        time: Milliseconds = 20000,
+        obj_id: str = "notifier") -> Command:
+    def new_icon(path: Path) -> ScriptBlockPS1:
+        assert path.exists()
+        assert path.is_file()
+
+        return f"[System.Drawing.Icon]::new('{str(path)}')"
+
+    def new_balloon_tip_icon(value: BalloonTip) -> ScriptBlockPS1:
+        return f"[System.Windows.Forms.ToolTipIcon]::{value}"
+
+    def set_visible(b: bool) -> ScriptBlockPS1:
+        return "$" + str(b).lower()
+
+    def script_block(*cmds: Command) -> ScriptBlockPS1:
+        return "{{{}}}".format(";".join(cmds))
+
+    # TODO: feed commands to powershell using a pipe
+    return "powershell.exe \"& {}\"".format(script_block(
+        # import the .NET class
+        "Add-Type -AssemblyName System.Windows.Forms",
+        # create the notification object
+        f"$global:{obj_id} = New-Object System.Windows.Forms.NotifyIcon",
+
+        # set the icons
+        f"${obj_id}.Icon = {new_icon(icon)}",
+        f"${obj_id}.BalloonTipIcon = {new_balloon_tip_icon("None")}",
+        # set the titles
+        f"${obj_id}.BalloonTipTitle = '{title}'",
+        f"${obj_id}.BalloonTipText = '{text}'",
+        # enable notification display
+        f"${obj_id}.Visible = {set_visible(True)}",
+
+        # set time in milliseconds and display the notification
+        f"${obj_id}.ShowBalloonTip({time})",
+    ))
